26dcdc7b25068310b1e196a2aee5aa5dca3fa8ee
Alexis <lxe.dubois@gmail.com>
2020-07-16 21:36:45 -0400

    Updated Coords

--- src/Model/Coords.java
+++ src/Model/Coords.java
@@ -1,4 +1,15 @@
 package Model;
 
 public class Coords {
+    int x;
+    int y;
+    public Coords(int x, int y){
+
+    }
+
+    public void setCoords(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
 }
